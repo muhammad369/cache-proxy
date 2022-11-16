042ce7c59f9e31b4df1ad11028eeabb7c954e760
Mohamed <mselim@voyancemed.com>
2022-11-16 17:34:29 +0200

    self hosted controller

--- CacheProxyMockServer/Program.cs
+++ CacheProxyMockServer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Avalonia;
 using Avalonia.ReactiveUI;
+using Microsoft.AspNetCore.Hosting;
 using System;
+using System.Diagnostics;
 
 namespace CacheProxyMockServer
 {
@@ -10,8 +12,25 @@ namespace CacheProxyMockServer
 		// SynchronizationContext-reliant code before AppMain is called: things aren't initialized
 		// yet and stuff might break.
 		[STAThread]
-		public static void Main(string[] args) => BuildAvaloniaApp()
+		public static void Main(string[] args)
+		{
+			// prevent more than one app instance
+			if (Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Length > 1)
+			{
+				return;
+			}
+			// server
+			var host = new WebHostBuilder()
+				.UseKestrel()
+				.UseUrls("http://*:1234")
+				.UseStartup<Startup>()
+				.Build();
+
+			host.Run();
+			// avalonia ui
+			BuildAvaloniaApp()
 			.StartWithClassicDesktopLifetime(args);
+		}
 
 		// Avalonia configuration, don't remove; also used by visual designer.
 		public static AppBuilder BuildAvaloniaApp()
---
+++ CacheProxyMockServer/ProxyController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace CacheProxyMockServer
+{
+	public class ProxyController: ControllerBase
+	{
+		[HttpGet]
+		[Route("/")]
+		public string Get()
+		{
+			return "Test";
+		}
+	}
+}
