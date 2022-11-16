298320d59169ec1820185856779773e6bab46111
Mohamed <mselim@voyancemed.com>
2022-11-16 16:51:09 +0200

    empty avalonia project

---
+++ CacheProxyMockServer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,8 @@
+﻿
+namespace CacheProxyMockServer.ViewModels
+{
+	public class MainWindowViewModel : ViewModelBase
+	{
+		public string Greeting => "Welcome to Avalonia!";
+	}
+}
---
+++ CacheProxyMockServer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace CacheProxyMockServer.ViewModels
+{
+	public class ViewModelBase : ReactiveObject
+	{
+	}
+}
