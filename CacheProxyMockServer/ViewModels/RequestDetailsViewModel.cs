9a538bd6fcc98ddd632e1aea321067bceb5cf20c
Mohamed <mselim@voyancemed.com>
2022-11-19 20:36:41 +0200

    RuleItemView - RequestDetailsView

---
+++ CacheProxyMockServer/ViewModels/RequestDetailsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheProxyMockServer.ViewModels
+{
+	public class RequestDetailsViewModel
+	{
+		public int Id { get; set; }
+
+		public string Method { get; set; }
+		public string Url { get; set; }
+		public string? RequestBody { get; set; }
+
+		public bool IsActive { get; set; } = true;
+		public bool IsUrlTemplate { get; set; } = false;
+		public bool IsBodyTemplate { get; set; } = false;
+
+		public int ResponseStatus { get; set; }
+		public string ResponseContent { get; set; }
+		public string ResponseHeaders { get; set; }
+	}
+}
---
+++ CacheProxyMockServer/Views/RuleItemView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CacheProxyMockServer.Views;
+
+public partial class RuleItemView : UserControl
+{
+    public RuleItemView()
+    {
+        InitializeComponent();
+    }
+}
